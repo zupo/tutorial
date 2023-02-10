a5686ff44667afca02116c9f40676446d050e179
Nejc Zupan <nejc.zupan@gmail.com>
2023-02-10 21:15:29 +0000

    helloworld

---
+++ shell.nix
@@ -0,0 +1,30 @@
+let
+  nixpkgs = builtins.fetchTarball {
+    # https://status.nixos.org/ -> nixos-22.11 on 2023-02-10
+    url = "https://github.com/nixos/nixpkgs/archive/49efda9011e8cdcd6c1aad30384cb1dc230c82fe.tar.gz";
+  };
+  pkgs = import nixpkgs {};
+  poetry2nix = import (fetchTarball {
+    # https://github.com/nix-community/poetry2nix/commits/master on 2023-02-10
+    url = "https://github.com/nix-community/poetry2nix/archive/860530598f2ab9a9a0e89dc79851d14b0aed3bf7.tar.gz";
+  }) {
+    pkgs = pkgs;
+  };
+
+  env = poetry2nix.mkPoetryEnv {
+    pyproject = ./pyproject.toml;
+    poetrylock = ./poetry.lock;
+    # editablePackageSources = {
+    #   trivial = ./src;
+    # };
+  };
+in
+
+pkgs.mkShell {
+  name = "dev-shell";
+  buildInputs = [
+    env
+    pkgs.poetry
+  ];
+
+}
